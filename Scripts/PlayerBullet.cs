388498ba057b060631c03a72eb508e91ad6b3ad1
MiggieeS <jmmsorianoii@gmail.com>
2026-01-12 08:31:18 +0800

    minor formatting changes

--- Scripts/PlayerBullet.cs
+++ Scripts/PlayerBullet.cs
@@ -4,7 +4,7 @@ using System;
 public partial class PlayerBullet : Area2D
 {
 	[Export]
-	public float Speed = 600f;
+	public float Speed = 600f; // double the player's speed
 
 	public Vector2 Direction = new Vector2(0, -1);
 
