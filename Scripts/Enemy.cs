0c376f57f23760be39dc153482c7c222f184ff9e
MiggieeS <jmmsorianoii@gmail.com>
2026-01-13 08:41:13 +0800

    added patterns to bullet hell mechanic from enemy

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class Enemy : Node2D
+{
+	[Export]
+	public PackedScene BulletScene;
+
+	[Export]
+	public float FireInterval = 0.7f;
+
+	[Export]
+	public int BulletsPerWave = 16;
+
+	private float _timer = 0f;
+	private float _angleOffset = 0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		_timer -= (float)delta;
+		if (_timer <= 0f)
+		{
+			_timer = FireInterval;
+			FireCirclePattern();
+		}
+	}
+
+	private void FireCirclePattern()
+	{
+		if (BulletScene == null)
+			return;
+
+		float angleStep = Mathf.Tau / BulletsPerWave;
+
+		for (int i = 0; i < BulletsPerWave; i++)
+		{
+			float angle = _angleOffset + angleStep * i;
+			Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+			EnemyBullet bullet = BulletScene.Instantiate<EnemyBullet>();
+			bullet.Position = GlobalPosition;
+			bullet.Velocity = dir * bullet.Speed;
+
+			GetTree().CurrentScene.AddChild(bullet);
+		}
+
+		_angleOffset += 0.15f; // slowly rotate the pattern
+	}
+}
